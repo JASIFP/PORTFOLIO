59c306286705a82e70ae3d5d2c9980c7cfdefa5d
JASIFP <jjas429@gmail.com>
2025-01-28 14:47:34 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -104,7 +104,6 @@
    /* Mobile Menu
     * ---------------------------------------------------- */ 
     const ssMobileMenu = function() {
-
         const toggleButton = document.querySelector('.mobile-menu-toggle');
         const mainNavWrap = document.querySelector('.main-nav-wrap');
         const siteBody = document.querySelector("body");
@@ -113,14 +112,17 @@
 
         toggleButton.addEventListener('click', function(event) {
             event.preventDefault();
-            toggleButton.classList.toggle('is-clicked');
-            siteBody.classList.toggle('menu-is-open');
+
+        // Only toggle mobile menu when below 800px
+            if (window.matchMedia('(max-width: 800px)').matches) {
+                toggleButton.classList.toggle('is-clicked');
+                siteBody.classList.toggle('menu-is-open');
+            }
         });
 
         mainNavWrap.querySelectorAll('.main-nav a').forEach(function(link) {
             link.addEventListener("click", function(event) {
-
-                // at 800px and below
+            // Only close mobile menu when below 800px
                 if (window.matchMedia('(max-width: 800px)').matches) {
                     toggleButton.classList.toggle('is-clicked');
                     siteBody.classList.toggle('menu-is-open');
@@ -129,15 +131,13 @@
         });
 
         window.addEventListener('resize', function() {
-
-            // above 800px
+        // Reset mobile menu on larger screens (above 800px)
             if (window.matchMedia('(min-width: 801px)').matches) {
                 if (siteBody.classList.contains('menu-is-open')) siteBody.classList.remove('menu-is-open');
                 if (toggleButton.classList.contains("is-clicked")) toggleButton.classList.remove("is-clicked");
             }
         });
-
-    }; // end ssMobileMenu
+    };
 
 
    /* Highlight active menu link on pagescroll
